da21c7fd4c35515b9a92a25830ff13a3b604cb1a
MonWas <65023903+MonWas@users.noreply.github.com>
2024-02-26 21:03:46 +0100

    create random color generator component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Accordion from './components/accordion';
+import RandomColor from './components/random-color-generator';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       {/*Accordion*/}
       <Accordion/>
+      {/*Random Color Generator*/}
+      <RandomColor/>
     </div>
   );
 }
