96a015912c31264c15675b159f9c53ff199c165c
MonWas <65023903+MonWas@users.noreply.github.com>
2024-02-28 20:45:50 +0100

    create and style image slider component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Accordion from './components/accordion';
+import ImageSlider from './components/image-slider';
 import RandomColor from './components/random-color-generator';
 import StarRating from './components/star-rating';
 
@@ -12,6 +13,10 @@ function App() {
       <RandomColor/>
       {/*Star Rating*/}
       <StarRating noOfStars={10}/>
+      {/*Image Slider*/}
+      <ImageSlider url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}/>
     </div>
   );
 }
